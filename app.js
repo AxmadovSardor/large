b67b3169aa2f23ef077b1ee38590c76e600b5da9
AxmadovSardor <axmadov6greenclass@gmail.com>
2025-04-29 19:41:33 +0500

    f

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbx3pEjswXNkjiyqUVwgWTCc1WPuuuYi-IgIguzkb29AxzDT2Dai7c1Jlg97OzZE8n8qBw/exec'
 
 const form = document.forms['form']
+function handleCredentialResponse(response) {
+    const data = jwt_decode(response.credential);
+    const email = data.email;
+    document.getElementById("content").innerHTML = `
+      <h2>Welcome, ${data.name}</h2>
+      <p>Email: ${email}</p>
+      <img src="${data.picture}" width="100" height="100"/>
+    `;
+    handleSubmit(email);
+  }
 
-const data = jwt_decode(response.credential);
-const email = data.email;
-
-form.addEventListener('submit', handleSubmit)
-
-function handleSubmit(e){
-    e.preventDefault()
+function handleSubmit(email){
+    if (email) e.preventDefault();
 
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
     .then(response => alert("Thank you! Form is submitted" ))
